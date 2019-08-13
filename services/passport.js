1b935fc7330d5b8501ef9f124155a95815327766
raywoolman <rayjwoolman@gmail.com>
2019-08-13 09:38:36 +0200

    finished handling of repeat user returning (create account if new, next- handle login for existing

--- services/passport.js
+++ services/passport.js
@@ -3,20 +3,27 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy
 const mongoose = require('mongoose')
 const keys = require('../config/keys')
 
-// Cannot require in Mongoose library directly as Mongoose sees that as trying to
-// create multiple schemas. In stead, declare variable to schema name
+// Cannot require in Mongoose library directly as Mongoose sees that as trying
+// to create multiple schemas. In stead, declare variable to schema name
 // specifically. When using mongoose.model, one argument means fetching data, 2
 // args (one Schema name and other Schema), means pushing data
 const User = mongoose.model('users')
 
-
-//new User = new instance in db based on Schema defines in Users.js
-//Populating GoogleId with data from Google profile
-//.save() method pushes to MongoDB. Otherwise it is only local
+// new User = new instance in db based on Schema defines in Users.js Populating
+// GoogleId with data from Google profile .save() method pushes to MongoDB.
+// Otherwise it is only local
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-  new User({googleId: profile.id}).save();
+  User.findOne({googleId: profile.id})
+    .then(existingUser => {
+      if (existingUser) {
+        //we already have a profile with that ID
+      } else {
+        //No record exists with this ID
+        new User({googleId: profile.id}).save();
+      }
+    })
 }))
