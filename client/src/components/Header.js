c7a69442f794a2f26a9d9a1d072d260ee4c54451
raywoolman <rayjwoolman@gmail.com>
2019-09-24 17:29:24 +0200

    connected Header component to store to check if user is logged in

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,22 +1,28 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
-    render() {
-        return (
-            <nav>
-                <div className="nav-wrapper light-green">
-                    <a className="left brand-logo">
-                        rakr.
+  render() {
+    console.log(this.props);
+    return (
+      <nav>
+        <div className="nav-wrapper light-green">
+          <a className="left brand-logo">
+            rakr.
           </a>
-                    <ul className="right">
-                        <li>
-                            <a>Login with Google</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        );
-    }
+          <ul className="right">
+            <li>
+              <a>Login with Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return {auth};
+}
+
+export default connect(mapStateToProps)(Header);
