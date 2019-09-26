4d78bab8537e272e760a5ba732fe130e00f844cc
raywoolman <rayjwoolman@gmail.com>
2019-09-26 17:03:18 +0200

    continued with configuration of stripe. Stripe API and Epress API are cooperating. To continue with hooking up credits to the state next

--- models/User.js
+++ models/User.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
-const { Schema } = mongoose;
+const {Schema} = mongoose;
 
 //Schema is to tell Mongoose what to expect when we are uploading data
 const userSchema = new Schema({
-    googleId: String
+  googleId: String,
+  credits: {
+    type: Number,
+    default: 0
+  }
 })
 
-//The Mongoose 'model class' emulates a collection in mongodb.
-//Mongoose will not overwrite existing database if named the same. 
+// The Mongoose 'model class' emulates a collection in mongodb. Mongoose will not
+// overwrite existing database if named the same.
 mongoose.model('users', userSchema);
