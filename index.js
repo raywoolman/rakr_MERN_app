dbe0f567ca8a8bc6fd9359acc90378336fc9f2f0
raywoolman <rayjwoolman@gmail.com>
2019-10-01 11:28:08 +0200

    configured express to handle accidental paths, and redirect towards front-end js file

--- index.js
+++ index.js
@@ -31,6 +31,15 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENVV ==='production') {
+    // Express will serve prod. assets (main.js / main.ss)
+    app.use(express.static('client/build'));
+    //Express will serve index.html if unrecognised route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirName, 'client', 'build', 'index.html'))
+    });
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT);
